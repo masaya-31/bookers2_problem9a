dea72ee872b33d6bf3f6c33e75836b6f1dcc0f8f
MasayaIto <94973345+masaya-31@users.noreply.github.com>
2023-07-18 05:21:04 +0000

    応用課題8a

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,9 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   has_one_attached :profile_image
 
+  has_many :messages, dependent: :destroy
+  has_many :entries, dependent: :destroy
+
   has_many :followers, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :followeds, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
 
